f356ace37565e69206b028919c5a2b05841675e0
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-10 01:50:59 +0700

    feat: some layout

---
+++ src/layouts/AuthLayout.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { useNavigate } from "react-router-dom";
+import { Loader, Sidebar } from "../components";
+
+const AuthLayout = ({ children, onLoad = () => {} }) => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const user = localStorage.getItem("user");
+
+    if (!user) {
+      navigate("/login");
+    } else {
+      setLoading(false);
+      onLoad();
+    }
+  }, [setLoading, navigate, onLoad]);
+
+  if (loading) {
+    return;
+  }
+
+  return (
+    <div className="container-fluid position-relative d-flex p-0">
+      <Loader />
+      <Sidebar />
+      {children}
+    </div>
+  );
+}
+
+export default AuthLayout;
