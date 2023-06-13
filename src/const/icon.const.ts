97880ec5f3cbde053c935cf371ce3da1efcac359
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-13 14:36:30 +0700

    feat!: unused code

--- src/const/icon.const.ts
+++
@@ -1,3 +0,0 @@
-export const IconDetail = {
-    Lead : "LEAD",
-}
--- src/models/control.metadata.ts
+++
@@ -1,7 +0,0 @@
-export class ControlMetaData {
-    Name: string;
-    Label: string;
-    LabelCss:string;
-    ControlCss: string;
-    Type:string;
-}
