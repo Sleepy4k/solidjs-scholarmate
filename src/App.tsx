1038204f3afdc7266a0160be8554dd0f2d378997
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-13 14:49:12 +0700

    chore: main src

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import { Component, createSignal, lazy } from 'solid-js';
+import { Component, lazy } from 'solid-js';
+import { Routes, Route } from "@solidjs/router";
 
-import { Router, Routes, Route } from "@solidjs/router";
-
-
-const Login = lazy(() => import("./pages/auth/login"));
-const Register = lazy(() => import("./pages/auth/Register"));
-const Dashboard = lazy(() => import("./pages/dash/dashboard"));
+// import pages
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
 
 const App: Component = () => {
   return (
-  <Router>
-      <Routes>
-        <Route path="/" component={Dashboard} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-      </Routes>
-  </Router>
-    
+    <Routes>
+      <Route path="/" component={Dashboard} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Routes>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
-/* @refresh reload */
 import './index.css';
-import { render } from 'solid-js/web';
-
 import App from './App';
+import { render } from 'solid-js/web';
 import { Router } from "@solidjs/router";
-const title = import.meta.env.VITE_APP_NAME || 'SolidJS';
+
 const root = document.getElementById('root');
+const title = import.meta.env.VITE_APP_NAME || 'SolidJS';
+
+if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
+  throw new Error(
+    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?',
+  );
+}
+
 document.title = title + ' Mates';
 
-render(() =>  (
-    <Router>
-      <App />
-    </Router>
-    ),
-document.getElementById('root') as HTMLElement);
+render(() => (
+  <Router>
+    <App />
+  </Router>
+), document.getElementById('root') as HTMLElement);
+
