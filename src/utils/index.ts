0f021cdeafc12752c2dd063e81fc4be32675a652
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-14 00:29:04 +0700

    chore: some util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
-import Api from "./Api";
 import Println from "./Debug";
 import { getCookie, checkCookie } from "./Cookie";
 import { getStorage, setStorage, deleteStorage } from "./LocalStorage";
 
-export { Api, Println, getCookie, checkCookie, getStorage, setStorage, deleteStorage };
+export { Println, getCookie, checkCookie, getStorage, setStorage, deleteStorage };
