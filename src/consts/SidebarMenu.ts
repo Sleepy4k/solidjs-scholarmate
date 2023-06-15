d47ead232fc715931a44c50f0022135a2caf5877
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-16 00:09:27 +0700

    chore: some file

--- src/consts/SidebarMenu.ts
+++ src/consts/SidebarMenu.ts
@@ -2,8 +2,8 @@ const SidebarMenu = [
   { title: "Dashboard", Icon: "DASHBOARD", Type: "MENU", link: "/", role: "any" },
   { title: "", Icon: "", Type: "DIVIDER", link: "", role: "any"},
   { title: "Student", Icon: "DASHBOARD", Type: "MENU", link: "/student", role: "any" },
-  { title: "Schoolarship", Icon: "DASHBOARD", Type: "MENU", link: "/schoolarship", role: "admin" },
-  { title: "University", Icon: "DASHBOARD", Type: "MENU", link: "/university", role: "admin" },
+  { title: "Schoolarship", Icon: "DASHBOARD", Type: "MENU", link: "/schoolarship", role: "any" },
+  { title: "University", Icon: "DASHBOARD", Type: "MENU", link: "/university", role: "any" },
 ]
 
 export default SidebarMenu;
--- src/pages/Schoolarship.tsx
+++ src/pages/Schoolarship.tsx
@@ -1,21 +1,50 @@
 import { Api } from "../services";
-import { Println } from "../utils";
 import { GridData } from "../components";
 import AuthLayout from "../layouts/AuthLayout";
+import { Println, getStorage } from "../utils";
 import { Component, createSignal } from "solid-js";
 
 const Schoolarship: Component = () => {
-  const field = [
+  const user = getStorage("user")
+  const [loading, setLoading] = createSignal(true);
+  const [schoolarship, setSchoolarship] = createSignal([]);
+  
+  let field = [
     { field: "id", headerName: "ID" },
     { field: "name" },
     { field: "major" },
     { field: "quantity" },
     { field: "description" },
-    { field: "requirement" }
+    { field: "requirement" },
+    { field: "action", cellRenderer: (params) => (
+      <>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Edit
+        </button>
+        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+          Delete
+        </button>
+      </>
+    )}
   ];
 
-  const [loading, setLoading] = createSignal(true);
-  const [schoolarship, setSchoolarship] = createSignal([]);
+  if (user.role !== "admin") {
+    field = [
+      { field: "id", headerName: "ID" },
+      { field: "name" },
+      { field: "major" },
+      { field: "quantity" },
+      { field: "description" },
+      { field: "requirement" },
+      { field: "action", cellRenderer: (params) => (
+        <>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Apply
+          </button>
+        </>
+      )}
+    ];
+  }
 
   const onFinish = () => {
     Api.get("schoolarship")
