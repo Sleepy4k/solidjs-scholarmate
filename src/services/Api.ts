6020424671ed12f79072dd8e83aece6589b317ca
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-16 23:00:32 +0700

    chore: api service

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -4,7 +4,14 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
-  withCredentials: true
+  withCredentials: true,
 })
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
