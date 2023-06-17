b92f175c5143b779e4f46b94356d016a028a9672
NaufalHidayat <90307751+NaufalHidayat@users.noreply.github.com>
2023-06-17 22:59:35 +0700

    update icon

--- src/consts/SidebarMenu.ts
+++ src/consts/SidebarMenu.ts
@@ -1,9 +1,9 @@
 const SidebarMenu = [
   { title: "Dashboard", Icon: "Category", Type: "MENU", link: "/", role: "any" },
   { title: "", Icon: "", Type: "DIVIDER", link: "", role: "any"},
-  { title: "Student", Icon: "Menu", Type: "MENU", link: "/student", role: "any" },
-  { title: "Schoolarship", Icon: "Menu", Type: "MENU", link: "/schoolarship", role: "any" },
-  { title: "University", Icon: "Menu", Type: "MENU", link: "/university", role: "any" },
+  { title: "Student", Icon: "Docs", Type: "MENU", link: "/student", role: "any" },
+  { title: "Schoolarship", Icon: "Brief", Type: "MENU", link: "/schoolarship", role: "any" },
+  { title: "University", Icon: "uni", Type: "MENU", link: "/university", role: "any" },
   { title: "", Icon: "", Type: "DIVIDER", link: "", role: "admin"},
   { title: "User", Icon: "Menu", Type: "MENU", link: "/user", role: "admin" },
 ]
