afdd68ed53523a8808e0ac0c82204c40a4c8d634
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-19 17:50:52 +0700

    chore: sidebar menu const

--- src/consts/SidebarMenu.ts
+++ src/consts/SidebarMenu.ts
@@ -2,7 +2,7 @@ const SidebarMenu = [
   { title: "Dashboard", Icon: "Category", Type: "MENU", link: "/", role: "any" },
   { title: "", Icon: "", Type: "DIVIDER", link: "", role: "any"},
   { title: "Student", Icon: "Docs", Type: "MENU", link: "/student", role: "any" },
-  { title: "Schoolarship", Icon: "Brief", Type: "MENU", link: "/schoolarship", role: "any" },
+  { title: "Scholarship", Icon: "Brief", Type: "MENU", link: "/scholarship", role: "any" },
   { title: "University", Icon: "uni", Type: "MENU", link: "/university", role: "any" },
   { title: "", Icon: "", Type: "DIVIDER", link: "", role: "admin"},
   { title: "User", Icon: "User", Type: "MENU", link: "/user", role: "admin" },
