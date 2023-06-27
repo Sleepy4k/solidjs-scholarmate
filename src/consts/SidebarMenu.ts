fdbb0fe3462f9c0fbb9588f37fcfa23eaaee3d2e
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-27 23:30:37 +0700

    chore: some const

--- src/consts/SidebarMenu.ts
+++ src/consts/SidebarMenu.ts
@@ -1,11 +1,11 @@
 const SidebarMenu = [
-  { title: "Dashboard", Icon: "Category", Type: "MENU", link: "/", role: "any" },
-  { title: "", Icon: "", Type: "DIVIDER", link: "", role: "any"},
-  { title: "Student", Icon: "Docs", Type: "MENU", link: "/student", role: "any" },
-  { title: "Scholarship", Icon: "Brief", Type: "MENU", link: "/scholarship", role: "any" },
-  { title: "University", Icon: "uni", Type: "MENU", link: "/university", role: "any" },
-  { title: "", Icon: "", Type: "DIVIDER", link: "", role: "admin"},
-  { title: "User", Icon: "User", Type: "MENU", link: "/user", role: "admin" },
-]
+  { title: 'Dashboard', Icon: 'Category', Type: 'MENU', link: '/', role: 'any' },
+  { title: '', Icon: '', Type: 'DIVIDER', link: '', role: 'any'},
+  { title: 'Student', Icon: 'Docs', Type: 'MENU', link: '/student', role: 'any' },
+  { title: 'Scholarship', Icon: 'Brief', Type: 'MENU', link: '/scholarship', role: 'any' },
+  { title: 'University', Icon: 'uni', Type: 'MENU', link: '/university', role: 'any' },
+  { title: '', Icon: '', Type: 'DIVIDER', link: '', role: 'admin'},
+  { title: 'User', Icon: 'User', Type: 'MENU', link: '/user', role: 'admin' },
+];
 
 export default SidebarMenu;
