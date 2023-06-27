41003d406739e211e4d560f0953cc51fbd172663
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-06-27 23:28:51 +0700

    chore: main file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,27 @@
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+import { dirname, resolve } from 'path';
 import solidPlugin from 'vite-plugin-solid';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@components': resolve(__dirname, './src/components'),
+      '@consts': resolve(__dirname, './src/consts'),
+      '@layouts': resolve(__dirname, './src/layouts'),
+      '@services': resolve(__dirname, './src/services'),
+      '@utils': resolve(__dirname, './src/utils'),
+    }
+  },
   server: {
-    host: true,
-    port: 3000
+    port: 3000,
   },
   build: {
     target: 'esnext',
