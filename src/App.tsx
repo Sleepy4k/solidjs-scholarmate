f0e903446813c8b673006aba14cc27869a56cf4c
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-05 06:13:53 +0700

    chore: main src

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ const Student = lazy(() => import('./pages/Student'));
 const Register = lazy(() => import('./pages/Register'));
 const Dashboard = lazy(() => import('./pages/Dashboard'));
 const University = lazy(() => import('./pages/University'));
+const StudentAdd = lazy(() => import('./pages/StudentAdd'));
 const Scholarship = lazy(() => import('./pages/Scholarship'));
+const StudentEdit = lazy(() => import('./pages/StudentEdit'));
 const UniversityEdit = lazy(() => import('./pages/UniversityEdit'));
 const ScholarshipEdit = lazy(() => import('./pages/ScholarshipEdit'));
 
@@ -19,10 +21,10 @@ const App: Component = () => {
       <Route path="/login" component={Login} />
       <Route path="/student" component={Student} />
       <Route path="/register" component={Register} />
-      <Route path="/student/add" component={Student} />
       <Route path="/university" component={University} />
+      <Route path="/student/add" component={StudentAdd} />
       <Route path="/scholarship" component={Scholarship} />
-      <Route path="/student/:id/edit" component={Student} />
+      <Route path="/student/:id/edit" component={StudentEdit} />
       <Route path="/university/:id/edit" component={UniversityEdit} />
       <Route path="/scholarship/:id/edit" component={ScholarshipEdit} />
     </Routes>
